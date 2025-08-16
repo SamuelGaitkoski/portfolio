9452f4f1bfa4d3169dea4ef360edf4c1a17e2587
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-16 02:01:27 -0300

    Format project files with Prettier.

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,30 +4,30 @@ export const headerData = {
   links: [
     {
       text: 'About',
-      href: getPermalink('/about')
+      href: getPermalink('/about'),
     },
     {
       text: 'Projects',
-      href: getPermalink('/projects')
+      href: getPermalink('/projects'),
     },
     {
       text: 'Contact',
-      href: getPermalink('/contact')
-    }
-  ]
+      href: getPermalink('/contact'),
+    },
+  ],
 };
 
 export const socialLinks = [
-  { 
+  {
     ariaLabel: 'LinkedIn',
     text: 'LinkedIn',
     icon: 'mdi:linkedin',
-    href: 'https://www.linkedin.com/in/samuel-gaitkoski/'
+    href: 'https://www.linkedin.com/in/samuel-gaitkoski/',
   },
-  { 
+  {
     ariaLabel: 'Github',
     text: 'Github',
     icon: 'mdi:github',
-    href: 'https://github.com/SamuelGaitkoski'
-  }
+    href: 'https://github.com/SamuelGaitkoski',
+  },
 ];
