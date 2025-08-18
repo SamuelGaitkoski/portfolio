57851a7c6dbdd8c7fcc516941dd1d4c752fd99a5
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-18 15:40:07 -0300

    Implement Express server in server.ts.

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import emailRouter from "./routes/email";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.POST || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/email", emailRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
