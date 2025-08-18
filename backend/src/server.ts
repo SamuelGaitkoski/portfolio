2cc27e4c984c0bf48e5549fd6a2fbd68d3776b63
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-18 17:16:49 -0300

    Integrate Swagger API documentation in server.ts.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import emailRouter from "./routes/email";
+import { setupSwagger } from "./swagger";
 
 dotenv.config();
 
@@ -13,6 +14,10 @@ app.use(express.json());
 // Routes
 app.use("/api/email", emailRouter);
 
+// Swagger API docs
+setupSwagger(app);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
