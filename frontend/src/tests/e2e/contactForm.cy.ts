a778fb08bbcd6fd68b70ba227b682f59264f5fc6
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-29 15:56:39 -0300

    Set up structure for contact form e2e tests in contactForm.cy.ts.

---
+++ frontend/src/tests/e2e/contactForm.cy.ts
@@ -0,0 +1,13 @@
+describe("Contact form", () => {
+  beforeEach(() => {
+    cy.visit("/contact");
+  });
+
+  it("submits successfully when filled correctly", () => {
+
+  });
+
+  it("shows error toast when API fails", () => {
+
+  });
+});
